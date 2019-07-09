0c9d61e399574ef72cc46badaf555717188ec96c
Solacx <junyong6@gmail.com>
2019-07-09 22:00:07 +1000

    Added template for Bullet sprite testing.

---
+++ test_sprite.py
@@ -0,0 +1,16 @@
+import logging
+import unittest
+
+import pygame
+
+
+class TestBullet(unittest.TestCase):
+
+    def setUp(self):
+        self.screen = pygame.display.set_mode((600,380))
+
+    # def test_update()
+    # def test_collision()
+
+
+if (__name__ == "__main__"): unittest.main()
